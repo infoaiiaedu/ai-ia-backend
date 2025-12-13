7b1384a55864a807c00e1a932a02b70ad9a29448
infoaiiaedu <infoaiiaedu@gmail.com>
2025-12-13 16:47:32 +0000

    feat: add server selection to Swagger API docs (production + local)

--- code/api/base.py
+++ code/api/base.py
@@ -3,7 +3,14 @@ from apps.core.api import router as content_router
 from apps.user.api import router as user_router
 from apps.payments.api import router as payments_router
 
-api = NinjaAPI(docs_url="docs/", csrf=False)
+api = NinjaAPI(
+    docs_url="docs/",
+    csrf=False,
+    servers=[
+        {"url": "https://api.eduaiia.com", "description": "Production Server"},
+        {"url": "http://localhost:8080", "description": "Local Development Server"},
+    ]
+)
 
 api.add_router("/payments/", payments_router)
 api.add_router("/content/", content_router)
