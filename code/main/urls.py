cb7ecb217f936a5291379f6ad59954351c8bbd09
ilia <i.kevkhishvili@palitra.ge>
2026-02-04 15:25:52 +0400

    added SITE_NAME

--- code/main/urls.py
+++ code/main/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView, RedirectView
 from api import api
+import settings
+
+admin.site.site_header = settings.SITE_NAME
+
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
